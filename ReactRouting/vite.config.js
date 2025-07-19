42b9726b658b20bd4afe0644d0d838abeea0fb7b
Pawan Rai <pr90446@gmail.com>
2025-07-19 14:12:33 +0545

    deploy all

--- ReactRouting/vite.config.js
+++ ReactRouting/vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
   base: "/React/ReactRouting/",
-=======
-  base: '/React/',
->>>>>>> 5e042a618163cda954c8b51c389d4c68dad3d900
 });
