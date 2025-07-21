57063d0c09df1bebb5b30e42ad4bb23739742bc7
Pawan Rai <pr90446@gmail.com>
2025-07-21 17:26:16 +0545

    Update package.json and index.html for correct asset paths and homepage

--- ReactRouting/vite.config.js
+++ ReactRouting/vite.config.js
@@ -6,4 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // base: "/React/ReactRouting/",
+  base: "/React/ReactRouting/",
 });
