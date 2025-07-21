9ff781d3e008438f682052f57fed99632184ff92
Pawan Rai <pr90446@gmail.com>
2025-07-21 12:37:19 +0545

    Form rebuild complete

--- ConditionalOperation/src/pages/RegLogForm.jsx
+++ ConditionalOperation/src/pages/RegLogForm.jsx
@@ -1,11 +1,21 @@
-// import "./form.css";
+import { useEffect, useState } from "react";
 const allType = ["register", "login"];
 const RegLogForm = ({ type }) => {
+  const [change, setChange] = useState(type);
+
+  useEffect(() => {
+    document.title = `${
+      change.charAt(0).toUpperCase() + change.slice(1)
+    } | ${document.title.replace(/^(Register|Login) \| /, "")}`;
+  }, [change]);
+
+  if (!allType.includes(change)) throw new Error("invalid type: " + change);
+
   const data = [
     {
       id: "name",
       name: "name",
-      type: "name",
+      type: "text",
       placeholder: "Enter your name",
     },
     {
@@ -27,75 +37,50 @@ const RegLogForm = ({ type }) => {
       placeholder: "Enter your password",
     },
   ];
-  if (!allType.includes(type)) throw new Error("invalid type: " + type);
   return (
-    <div className="flex items-center justify-center h-[100vh] bg-gray-400">
-      <div className="border-1 rounded-2xl p-10">
-        <div className="flex flex-col">
-          <div className="text-2xl capitalize">{type}</div>
-          {data
-            .filter((item) => {
-              if (type === "login") {
-                return item.name === "email" || item.name === "password";
-              }
-              return true; // for register, return all fields
-            })
-            .map((item, index) => (
-              <div
-                className="flex flex-col gap-1 pt-2 border-red-500 "
-                key={index}
-              >
-                <label key={index} htmlFor={item.id} className="capitalize">
-                  {item.name}
-                </label>
-                <input
-                  type={item.type}
-                  id={item.id}
-                  name={item.name}
-                  placeholder={item.placeholder}
-                  className="border-2 rounded-lg p-2"
-                />
-              </div>
-            ))}
-          {/* {type === "register" && (
-            <>
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" placeholder="Enter your name" />
-              <label htmlFor="phone">Phone:</label>
-              <input
-                type="tel"
-                name=""
-                id="phone"
-                onChange={() => console.log("hello")}
-                placeholder="Enter your phone number"
-              />
-            </>
-          )}
-
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name=""
-            id="email"
-            placeholder="Enter your email"
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name=""
-            id="password"
-            placeholder="Create your password"
-          /> */}
+    <div>
+      <div className="flex items-center justify-center h-[100vh] bg-gray-400">
+        <div className="border-1 rounded-2xl p-10">
+          <div className="flex flex-col">
+            <div className="text-2xl capitalize">{change}</div>
+            {data
+              .filter((item) => {
+                if (change === "login") {
+                  return item.name === "email" || item.name === "password";
+                }
+                return true; // for register, return all fields
+              })
+              .map((item, index) => (
+                <div
+                  className="flex flex-col gap-1 pt-2 border-red-500 "
+                  key={index}
+                >
+                  <label key={index} htmlFor={item.id} className="capitalize">
+                    {item.name}
+                  </label>
+                  <input
+                    type={item.type}
+                    id={item.id}
+                    name={item.name}
+                    placeholder={item.placeholder}
+                    className="border-2 rounded-lg p-2"
+                  />
+                </div>
+              ))}
+          </div>
+          <button className="bg-blue-500 capitalize py-2 w-25 rounded-2xl mt-7 mx-auto grid place-content-center hover:bg-blue-300 active:bg-blue-800">
+            {change}
+          </button>
         </div>
-        <button className="bg-blue-500 capitalize py-2 w-25 rounded-2xl mt-7 mx-auto grid place-content-center hover:bg-blue-300 active:bg-blue-800">
-          {type}
-        </button>
       </div>
+      <button
+        onClick={() => setChange(change === "login" ? "register" : "login")}
+        className="absolute top-0 right-0 m-4 p-2 bg-gray-200 rounded hover:bg-gray-300 active:bg-gray-400 w-[150px] text-left"
+      >
+        Change: {change}
+      </button>
     </div>
   );
 };
 
 export default RegLogForm;
-RegLogForm.prototype = {
-  type: "login" | "register",
-};
